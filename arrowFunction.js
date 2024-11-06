025aecf3032ba200b12aa584be030e98443f16e6
Wejdan Khaled <vauseq8@icloud.com>
2024-11-06 17:46:32 +0300

    done

---
+++ arrowFunction.js
@@ -0,0 +1,8 @@
+const greet = (name) =>  `Hello, ${name}!`;
+//console.log(greet("Wejdan"));
+
+const sum = (a, b) => a + b;
+//console.log(sum(5, 3));
+
+const square = (number) => number **2;
+//console.log(square(5));
